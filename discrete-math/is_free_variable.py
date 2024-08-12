0df5fe46f977c0a405c02fbf9f972a653e9b13d4
Alex Baranov <alexander05baranov@yandex.ru>
2024-08-12 17:07:46 +0300

    Add discrete theme

---
+++ discrete-math/is_free_variable.py
@@ -0,0 +1,41 @@
+from math import log2
+from itertools import product
+
+#input
+conjunction = [0, 0, 0, 1] 
+disjunction = [0, 1, 1, 1]
+my_function = [0, 0, 1, 1]
+
+def is_free_variable(func_vector: list[bool], pos: int) -> bool:
+
+    size = log2(len(func_vector))
+    pos -= 1
+
+    if not size.is_integer():
+        return "Bad args: len(func_vector) != 2, 4, 8, 16 ..."
+    elif pos >= size:
+        return "Out of range of args"
+
+    tmp_args = list(map(''.join, product(['0', '1'], repeat=int(size) - 1)))[::-1]     
+
+    for tmp_arg in tmp_args:
+        first_args = tmp_arg[:pos] + '0' + tmp_arg[pos:] #01010_0_0101 ...
+        second_args = tmp_arg[:pos] + '1' + tmp_arg[pos:] #01010_1_0101 ...
+
+        n1 = int(first_args, 2)
+        n2 = int(second_args, 2)
+
+        if func_vector[n1] != func_vector[n2]:
+            return False
+        
+    return True
+    
+
+print(is_free_variable(conjunction, 2))
+print(is_free_variable(disjunction, 2))
+print(is_free_variable(my_function, 2))
+
+#output
+# False
+# False
+# True
